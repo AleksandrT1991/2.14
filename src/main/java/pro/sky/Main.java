24acdab231d58dd13083a7d50dae7b5bd7031704
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2022-07-24 16:28:37 +0300

    2.14

--- src/main/java/pro/sky/Main.java
+++ src/main/java/pro/sky/Main.java
@@ -1,7 +1,19 @@
+package pro.sky;
 
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        StringList stringList = new StringListImpl();
+        stringList.add("e1");
+        stringList.add("e2");
+        stringList.add("e3");
+        stringList.add("e4");
+        stringList.add(1, "e6");
+        System.out.println(Arrays.toString(stringList.toArray()));
+        stringList.clear();
+        System.out.println(stringList.size());
+
+
     }
 }
